743f9c7a27e60ba88f899a96c347af0910d881cc
chisaki-kajiya <chisaki.kajiya21@gmail.com>
2022-12-18 02:29:20 +0900

    EDIT thanks.blade.php

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -3,8 +3,8 @@
 @section('title', '会員登録完了')
 
 @section('content')
-<div class="box__contents">
+<div class="box__contents flex-column">
   <h2 class="box__contents-text">会員登録ありがとうございます</h2>
-  <button class="box__contents-btn center mb50">ログインする</button>
+  <a class="box__contents-btn center mb50" href="/login">ログインする</a>
 </div>
 @endsection
